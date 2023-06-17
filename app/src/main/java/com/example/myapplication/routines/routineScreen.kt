fca0fb772b65aa6f0af7740819f423932b5c26e9
brufarITBA <bfarbiarz@gmail.com>
2023-06-17 13:34:42 -0300

    routines multilang

--- app/src/main/java/com/example/myapplication/routines/routineScreen.kt
+++ app/src/main/java/com/example/myapplication/routines/routineScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,7 +57,7 @@ fun RoutinesScreen(
         ) {
             Column {
                 Text(
-                    text = "My Routines",
+                    text = stringResource(id = R.string.routinesHeader),
                     style = MaterialTheme.typography.headlineMedium,
                     color = Color.White,
                     modifier = Modifier.padding(16.dp)
